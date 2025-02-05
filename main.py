2ca0cb75a1339f5687712e02576015e8be1b0b5a
hezonglianheng <976410976@qq.com>
2025-02-05 22:47:05 +0800

    添加对古代汉语语料库的检索

--- main.py
+++ main.py
@@ -18,15 +18,16 @@ def read_query_file() -> Iterator[str]:
         for line in f:
             yield line.strip()
 
-async def word_match(word: str, file_path: Path):
+async def word_match(word: str, file_path: Path, ancestor_path: Path):
     """查找语料库中的匹配词，并将匹配的文本片段写入文件
 
     Args:
         word (str): 待匹配的词
         file_path (Path): 语料文件路径
+        ancestor_path (Path): 语料库根目录路径
     """
     # 去除file_path中的前缀路径
-    output_path = file_path.relative_to(config.CORPUS_PATH)
+    output_path = file_path.relative_to(ancestor_path)
     # 获得一系列匹配的文本片段
     matched: list[str] = []
     try:
@@ -58,7 +59,25 @@ def sub_corpus_match(word: str, sub_path: str):
         files = list(Path(config.CORPUS_PATH / sub_path).rglob("*.txt"))
 
         for batch in [files[i:i + config.BATCH_SIZE] for i in range(0, len(files), config.BATCH_SIZE)]:
-            batch_tasks = [word_match(word, file_path) for file_path in batch]
+            batch_tasks = [word_match(word, file_path, config.CORPUS_PATH) for file_path in batch]
+            tasks.extend(batch_tasks)
+
+        await asyncio.gather(*tasks)
+
+    asyncio.run(process_files())
+
+def ancient_corpus_match(word: str):
+    """查找古代语料库中的匹配词，并将匹配的文本片段写入文件
+
+    Args:
+        word (str): 待匹配的词
+    """
+    async def process_files():
+        tasks = []
+        files = list(config.ANCIENT_CORPUS_PATH.rglob("*.txt"))
+
+        for batch in [files[i:i + config.BATCH_SIZE] for i in range(0, len(files), config.BATCH_SIZE)]:
+            batch_tasks = [word_match(word, file_path, config.ANCIENT_CORPUS_PATH) for file_path in batch]
             tasks.extend(batch_tasks)
 
         await asyncio.gather(*tasks)
@@ -66,12 +85,15 @@ def sub_corpus_match(word: str, sub_path: str):
     asyncio.run(process_files())
 
 def main():
+    """程序主函数
+    """
     # 清空输出文件夹
     for file in Path(config.OUTPUT_DIR).rglob("*.txt"):
         file.unlink()
     # 读取查询词集文件
     for word, subpath in product(read_query_file(), config.SUB_PATHS):
-        sub_corpus_match(word, subpath)
+        ancient_corpus_match(word) # 先查找古代语料库
+        sub_corpus_match(word, subpath) # 再查找现代语料库
 
 if __name__ == "__main__":
     main()
