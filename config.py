183a27245d6b9487884b68ecdb466f11c276d73a
hezonglianheng <976410976@qq.com>
2025-02-05 18:05:57 +0800

    建立运行脚本文件

---
+++ config.py
@@ -0,0 +1,39 @@
+# encoding: utf8
+
+from pathlib import Path
+
+# encodings.
+CORPUS_ENCODING = "gbk"
+QUERY_ENCODING = "utf8"
+OUTPUT_ENCODING = "utf8"
+
+# files.
+QUERY_FILE = Path(__file__).parent / "words_set_small.txt" # 查询词集文件路径，请勿改动
+CORPUS_PATH = Path(__file__).parents[1] / "整理后语料V3/整理后语料V3/xiandai" # 现代汉语部分语料库路径，请勿改动
+
+# output.
+OUTPUT_DIR = Path(__file__).parent / "query_result" # 输出文件夹路径，请勿改动
+
+# 查找范围，与CCL在线查找的时间范围一致
+SUB_PATHS = [
+    "1900s",
+    "1910s", 
+    "1920s",
+    "1930s",
+    "1940s",
+    "1950s",
+    "1960s",
+    "1970s",
+    "1980s",
+    "1990s",
+    "2000s",
+    "2010s",
+    "2020s", 
+    "CWAC", 
+]
+
+# 显示范围
+DISPLAY_WINDOW = 50
+
+# batch size.
+BATCH_SIZE = 100
---
+++ main.py
@@ -0,0 +1,77 @@
+# encoding: utf8
+
+import config
+import aiofiles
+import asyncio
+from collections.abc import Iterator
+from pathlib import Path
+import re
+from itertools import product
+
+def read_query_file() -> Iterator[str]:
+    """读取查询词集文件
+
+    Yields:
+        Iterator[str]: 查询词
+    """
+    with open(config.QUERY_FILE, "r", encoding=config.QUERY_ENCODING) as f:
+        for line in f:
+            yield line.strip()
+
+async def word_match(word: str, file_path: Path):
+    """查找语料库中的匹配词，并将匹配的文本片段写入文件
+
+    Args:
+        word (str): 待匹配的词
+        file_path (Path): 语料文件路径
+    """
+    # 去除file_path中的前缀路径
+    output_path = file_path.relative_to(config.CORPUS_PATH)
+    # 获得一系列匹配的文本片段
+    matched: list[str] = []
+    try:
+        async with aiofiles.open(file_path, "r", encoding=config.CORPUS_ENCODING) as f:
+            content = await f.read()
+            for match in re.finditer(word, content):
+                start_position = max(0, match.start() - config.DISPLAY_WINDOW)
+                end_position = min(len(content), match.end() + config.DISPLAY_WINDOW)
+                matched_slice = content[start_position:match.start()] + "【" + content[match.start():match.end()] + "】" + content[match.end():end_position]
+                matched.append(matched_slice)
+    except Exception as e:
+        print(f"读取{file_path}失败: {e}")
+    # 将匹配的文本片段写入文件
+    if len(matched) > 0:
+        with open(config.OUTPUT_DIR / (word + ".txt"), "a", encoding=config.OUTPUT_ENCODING) as f:
+            for line in matched:
+                f.write(f"【来源: {output_path}】" + line + "\n")
+                print(f"在{output_path}中找到匹配词“{word}”")
+
+def sub_corpus_match(word: str, sub_path: str):
+    """查找语料库中的匹配词，并将匹配的文本片段写入文件
+
+    Args:
+        word (str): 待匹配的词
+        sub_path (str): 语料库的子路径
+    """
+    async def process_files():
+        tasks = []
+        files = list(Path(config.CORPUS_PATH / sub_path).rglob("*.txt"))
+
+        for batch in [files[i:i + config.BATCH_SIZE] for i in range(0, len(files), config.BATCH_SIZE)]:
+            batch_tasks = [word_match(word, file_path) for file_path in batch]
+            tasks.extend(batch_tasks)
+
+        await asyncio.gather(*tasks)
+
+    asyncio.run(process_files())
+
+def main():
+    # 清空输出文件夹
+    for file in Path(config.OUTPUT_DIR).rglob("*.txt"):
+        file.unlink()
+    # 读取查询词集文件
+    for word, subpath in product(read_query_file(), config.SUB_PATHS):
+        sub_corpus_match(word, subpath)
+
+if __name__ == "__main__":
+    main()
