863130651330bbe0428c3bde90599411e8ed74b0
hezonglianheng <976410976@qq.com>
2025-02-08 11:28:07 +0800

    支持Windows平台和Linux平台

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import config
 from pathlib import Path
 import subprocess
+import platform # 用于判断操作系统
 
 # 子进程数量
 sub_process_num = 8
@@ -26,7 +27,14 @@ def main():
     # 创建子进程
     processes = []
     for i in range(sub_process_num):
-        processes.append(subprocess.Popen(["python", "process.py", f"temp_batch/batch_{i}.txt"]))
+        if (curr_system := platform.system()) == "Windows":
+            processes.append(subprocess.Popen(["python", "process.py", f"temp_batch/batch_{i}.txt"]))
+        elif curr_system == "Linux":
+            # 在Linux下使用nohup命令使子进程离线运行，使用&符号使子进程在后台运行
+            processes.append(subprocess.Popen(["nohup", "python", "process.py", f"temp_batch/batch_{i}.txt", "&"]))
+        else:
+            print(f"暂不支持操作系统{curr_system}，请联系开发者")
+            return
     # 等待子进程结束
     for process in processes:
         process.wait()
