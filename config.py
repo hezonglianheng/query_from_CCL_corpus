86f808f7f7fe42713580d6005a837a0036b8c80d
hezonglianheng <22000014125@stu.pku.edu.cn>
2025-02-21 12:22:35 +0000

    根据情况修改语料库的路径和输出的路径

--- config.py
+++ config.py
@@ -10,11 +10,12 @@ OUTPUT_ENCODING = "utf8"
 
 # files.
 QUERY_FILE = Path(__file__).parent / "words_set.txt" # 查询词集文件路径，请勿改动
-CORPUS_PATH = Path(__file__).parents[1] / "整理后语料V3/整理后语料V3/xiandai" # 现代汉语部分语料库路径，请勿改动
-ANCIENT_CORPUS_PATH = Path(__file__).parents[1] / "整理后语料V3/整理后语料V3/gudai" # 古代汉语部分语料库路径，请勿改动
+# 语料库的路径，需要根据文件的情况修改
+CORPUS_PATH = Path(__file__).parents[1] / "storage/整理后语料V3/整理后语料V3/xiandai" # 现代汉语部分语料库路径，请勿改动
+ANCIENT_CORPUS_PATH = Path(__file__).parents[1] / "storage/整理后语料V3/整理后语料V3/gudai" # 古代汉语部分语料库路径，请勿改动
 
 # output.
-OUTPUT_DIR = Path(__file__).parent / "query_result" # 输出文件夹路径，请勿改动
+OUTPUT_DIR = Path(__file__).parents[1] / "storage/query_result" # 输出文件夹路径，请勿改动
 
 # 查找范围，与CCL在线查找的时间范围一致
 SUB_PATHS = [
